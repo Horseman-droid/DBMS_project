2fb65dfda115cb5e08599287c3aff4cef2297cc9
Devang Bharti <75468783+Horseman-droid@users.noreply.github.com>
2021-05-05 18:45:38 +0530

    Update adminhead.php

--- adminhead.php
+++ adminhead.php
@@ -18,8 +18,6 @@
 
     <!-- Custom Fonts -->
     <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/ 5shiv/3.7.0/ 5shiv.js"></script>
