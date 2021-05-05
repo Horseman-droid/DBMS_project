7888ae61b3417f7f30f1f7eb91e5de88e375c483
Devang Bharti <75468783+Horseman-droid@users.noreply.github.com>
2021-05-05 18:53:08 +0530

    Update index.php

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
 	<div class="row">
 		<div class="col-lg-12">
 			<h1 class="page-header">
-                    Welcome to Virtual CLASSROOM
+                    Welcome to Virtual Classroom
                 </h1>
 		
 		</div>
